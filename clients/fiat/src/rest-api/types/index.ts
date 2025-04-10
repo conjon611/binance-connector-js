5a853acf649d4ce81f7e1cefbb85202d6fb85c36
Dimitris <191377322+dimitrisn442@users.noreply.github.com>
2025-04-10 16:10:52 +0300

    Release fiat v2.0.0 (#292)
    Co-authored-by: github-actions <github-actions@github.com>

--- clients/fiat/src/rest-api/types/index.ts
+++ clients/fiat/src/rest-api/types/index.ts
@@ -1,4 +1,3 @@
-export * from './bad-request';
 export * from './get-fiat-deposit-withdraw-history-response';
 export * from './get-fiat-deposit-withdraw-history-response-data-inner';
 export * from './get-fiat-payments-history-response';
