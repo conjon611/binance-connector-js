aa917f9fa147dd86c44de52a0b6f9fb755c94df8
Dimitris <191377322+dimitrisn442@users.noreply.github.com>
2025-05-28 18:15:46 +0300

    Release pay v3.0.0 (#344)
    Co-authored-by: github-actions <github-actions@github.com>

--- clients/pay/CHANGELOG.md
+++ clients/pay/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## 3.0.0 - 2025-05-28
+
+### Changed (1)
+
+- Marked as signed the following endpoints:
+  - `GET /sapi/v1/pay/transactions`
+
 ## 2.0.0 - 2025-04-10
 
 ### Changed
--- clients/pay/src/rest-api/modules/pay-api.ts
+++ clients/pay/src/rest-api/modules/pay-api.ts
@@ -293,7 +293,7 @@ export class PayApi implements PayApiInterface {
             localVarAxiosArgs.method,
             localVarAxiosArgs.params,
             localVarAxiosArgs?.timeUnit,
-            { isSigned: false }
+            { isSigned: true }
         );
     }
 }
